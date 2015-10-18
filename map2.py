92188dcaa191cc99a34331a6562d6e3034390a73
Jeremy Barisch-Rooney <barischj@tcd.ie>
2015-10-18 12:01:22 +0100

    socket fun

--- map2.py
+++ map2.py
@@ -5,8 +5,8 @@ import sys
 import threading
 import time
 
-MASTER_PORT = 8060
-WORKER_PORT = 3061
+MASTER_PORT = 8002
+WORKER_PORT = 8001
 
 BROADCAST_MSG = "Broadcast"
 REGISTER_TIMEOUT = 1
@@ -38,6 +38,10 @@ class Master:
     self.workers = set() # Allow no duplicates.
     self.RegisterWorkers()
     print str(len(self.workers)), "workers found."
+    if not len(self.workers):
+      print "Exiting"
+      return
+    self.sock.close()
 
     amount_lines = len(lines)
     print "amount_lines: " + str(amount_lines)
@@ -47,25 +51,34 @@ class Master:
     print "lines_per_worker", lines_per_worker
     line_remainders = amount_lines % amount_workers
 
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind((broadcast_ip, MASTER_PORT))
+    print "listening for", len(self.workers), "workers"
+    s.listen(len(self.workers))
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    data = conn.recv(1024)
+    print data
+
     # Send all data to Workers.
-    lines_index = 0
-    for worker in self.workers:
-      self.sock.sendto(WORKERS_PREFIX + json.dumps(list(self.workers)), worker)
-      self.sock.sendto(MAP_PREFIX + map_f, worker)
-      # send partition_f
-      # send reduce_f
-      print "lines_index: " + str(lines_index)
-      lines_end_index = lines_index + lines_per_worker - 1
-      if line_remainders:
-        lines_end_index += 1
-        line_remainders -= 1
-      print "lines_end_index: " + str(lines_end_index)
-      counter = 0
-      for line in lines[lines_index:lines_end_index]:
-        self.sock.sendto(LINES_PREFIX + line, worker)
-        counter += 1
-      print "counter:", counter
-      lines_index = lines_end_index + 1
+    # lines_index = 0
+    # for worker in self.workers:
+    #   self.sock.sendto(WORKERS_PREFIX + json.dumps(list(self.workers)), worker)
+    #   self.sock.sendto(MAP_PREFIX + map_f, worker)
+    #   # send partition_f
+    #   # send reduce_f
+    #   print "lines_index: " + str(lines_index)
+    #   lines_end_index = lines_index + lines_per_worker - 1
+    #   if line_remainders:
+    #     lines_end_index += 1
+    #     line_remainders -= 1
+    #   print "lines_end_index: " + str(lines_end_index)
+    #   counter = 0
+    #   for line in lines[lines_index:lines_end_index]:
+    #     self.sock.sendto(LINES_PREFIX + line, worker)
+    #     counter += 1
+    #   print "counter:", counter
+    #   lines_index = lines_end_index + 1
 
     # Wait for all workers to finish.
 
@@ -94,16 +107,18 @@ class Worker:
     self.master = None
     self.Register()
 
+    s = create_connection(self.master, timeout=10)
+    s.send("JIMMY")
     # Receive all needed data from Master.
-    self.map_f = None
-    self.workers = None
-    self.lines = []
-    self.ReceiveData()
+    # self.map_f = None
+    # self.workers = None
+    # self.lines = []
+    # self.ReceiveData()
 
-    print "workers:", self.workers
-    print "len(map_f):", len(self.map_f)
-    print "len(lines):", len(self.lines)
-    print self.lines
+    # print "workers:", self.workers
+    # print "len(map_f):", len(self.map_f)
+    # print "len(lines):", len(self.lines)
+    # print self.lines
 
   def Register(self):
     registered = False
